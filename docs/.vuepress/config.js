803f57f0d2ea4bde5d45ae1174d6610feb8fd07d
thunderbiscuit <thunderB@protonmail.com>
2020-05-03 14:59:25 -0400

    docs: add config file + website MVP

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "VuePress Starter Template",
+  description: "Starter Template for Documentation Website",
+  base: "/vuepress-starter-template/",
+  themeConfig: {
+    navbar: true,
+    nav: [
+      { text: "GitHub", link: ""},
+      { text: "Website", link: ""}
+    ],
+  }
+}
